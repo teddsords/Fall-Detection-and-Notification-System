5b88ed75b7046e749419f1123fd782eebc8f64b3
Teddy Ordoñez <teddyonu@hotmail.com>
2021-08-20 12:07:21 -0300

    Testing button functionality working

--- App main.py
+++ App main.py
@@ -7,6 +7,7 @@ from RegisterContactDialog import Ui_Register_Contact_Dialog
 
 #importing libraries
 import sys
+import requests
 
 class CreditsDialog(QDialog):
     def __init__(self):
@@ -15,8 +16,11 @@ class CreditsDialog(QDialog):
         self.ui.setupUi(self)
 
         # Defining function to be called when the close button is clicked
-        self.ui.close_pushButton.clicked.connect(self.reject)
-        
+        self.ui.close_pushButton.clicked.connect(self.hideCreditsDialog)
+
+    # Function to     
+    def hideCreditsDialog(self):
+        self.hide()
 
 class RegisterContactDialog(QDialog):
     def __init__(self):
@@ -25,10 +29,25 @@ class RegisterContactDialog(QDialog):
         self.ui.setupUi(self)  
 
         # Defining function to be called when the close button is clicked
-        self.ui.cancel_pushButton.clicked.connect(self.reject)     
+        self.ui.cancel_pushButton.clicked.connect(self.hideRegisterContactDialog)
+        self.ui.test_pushButton.clicked.connect(self.testTelegramAlert) 
+
+    # Function to hide Register Contact Dialog
+    def hideRegisterContactDialog(self):
+        self.hide()
+
+    # Function to test the Chat ID
+    def testTelegramAlert(self):
+        message = 'Testing the Python script to send a private message'
+        file = open('Telegram Token.txt', 'r')
+        token = file.readline()
+        telegramID = self.ui.telegramID_lineEdit.text()
+        URL = 'https://api.telegram.org/bot{}/sendMessage?chat_id={}&text={}&parse_mode=HTML'.format(token, telegramID, message)
+        resp = requests.get(URL)
+        print(resp.text)
+        
 
 class MainWindow(QMainWindow):
-    
     def __init__(self):
         super(MainWindow,self).__init__()
         self.ui = Ui_MainWindow()
@@ -37,8 +56,9 @@ class MainWindow(QMainWindow):
         # Defining functions to be called whenever a button is clicked
         self.ui.credits_pushButton.clicked.connect(self.openCreditsDialog)
         self.ui.registerPerson_pushButton.clicked.connect(self.openRegisterContactDialog)
+        self.ui.close_pushButton.clicked.connect(self.closeMainwindow)
 
-
+    # Functions to open other Dialogs
     def openCreditsDialog(self):
         credits = CreditsDialog()
         credits.show()
@@ -48,12 +68,13 @@ class MainWindow(QMainWindow):
 
     def openRegisterContactDialog(self):
         registerContact = RegisterContactDialog()
-        registerContact.show()
 
         if registerContact.exec_():
             sys.exit(registerContact.exec_())
 
-
+    # Function to close Main Window
+    def closeMainwindow(self):
+        self.close()
 
 
 if __name__ == "__main__":
@@ -61,4 +82,3 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
-    
