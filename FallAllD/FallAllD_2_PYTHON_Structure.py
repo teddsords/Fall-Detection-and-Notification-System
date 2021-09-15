aaef8579c859e49dd68a4f276201b3be1a023a4f
Teddy Ordoñez <teddyonu@hotmail.com>
2021-09-15 11:08:14 -0300

    Found New Dataset!!!

---
+++ FallAllD/FallAllD_2_PYTHON_Structure.py
@@ -0,0 +1,66 @@
+# FallAllD files to Python struct
+# By By Majd SALEH 08-April-2020.
+import os
+from numpy import genfromtxt
+import numpy as np
+import pandas as pd 
+
+oldDir=os.getcwd()
+print(oldDir)
+ParentDir=os.getcwd()
+print(ParentDir)
+FP= ParentDir+"\\FallAllD\\"
+os.chdir(FP)
+
+FileNamesAll=os.listdir(FP)
+FileNames=[]
+for f_name in FileNamesAll:
+    if f_name.endswith('_A.dat'):
+        FileNames.append(f_name)
+LL=len(FileNames)
+
+l_SubjectID=[]
+l_Device=[]
+l_ActivityID=[]
+l_TrialNo=[]
+l_Acc=[]
+l_Gyr=[]
+l_Mag=[]
+
+for i in range(LL):
+    f_name=FileNames[i]
+    SubjectID=int(f_name[1:3])    
+    l_SubjectID.append(np.uint8(SubjectID))
+    ActivityID=int(f_name[8:11])    
+    l_ActivityID.append(np.uint8(ActivityID))
+    TrialNo=int(f_name[13:15])    
+    l_TrialNo.append(np.uint8(TrialNo))
+    Device=''
+    if(int(f_name[5])==3):
+        Device='Waist'
+    l_Device.append(Device)
+    
+    l_Acc.append(np.int16(genfromtxt(f_name, delimiter=',')))
+    chArr=list(f_name)
+    chArr[16]='G'
+    f_name="".join(chArr)    
+    l_Gyr.append(np.int16(genfromtxt(f_name, delimiter=',')))
+    chArr=list(f_name)
+    chArr[16]='M'
+    f_name="".join(chArr)    
+    l_Mag.append(np.int16(genfromtxt(f_name, delimiter=',')))
+    print(f'File  {i+1}  out of {len(FileNames)}')
+os.chdir(oldDir)
+
+
+FallAllD = pd.DataFrame(list(zip(l_SubjectID,l_Device,l_ActivityID,l_TrialNo,l_Acc,l_Gyr,l_Mag)), 
+               columns =['SubjectID', 'Device','ActivityID','TrialNo','Acc','Gyr','Mag']) 
+
+FallAllD.to_pickle('FallAllD.pkl')
+FallAllD.to_csv('FallAllD.csv')
+#FallAllD.to_hdf('FallAllD.h5', key='df', mode='w')
+
+# to import data use:
+#FallAllD = pd.read_pickle('FallAllD.pkl')
+#FallAllD = pd.read_hdf('FallAllD.h5', 'df')
+#print(FallAllD.info())
---
+++ FallAllD/Using FallAllD Dataset.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot
+
+# to import data use:
+FallAllD = pd.read_pickle('FallAllD.pkl')
+#FallAllD = pd.read_hdf('FallAllD.h5', 'df')
+print(FallAllD.info())
+indexToPlot = 1794
+sensor = 2  # Use 1 for Acc, 2 for Gyr and 3 for Mag
+print(FallAllD.loc[indexToPlot])
+
+if sensor == 1:
+    toPlotX = pd.DataFrame(FallAllD.loc[indexToPlot].Acc.tolist())[0]
+    toPlotY = pd.DataFrame(FallAllD.loc[indexToPlot].Acc.tolist())[1]
+    toPlotZ = pd.DataFrame(FallAllD.loc[indexToPlot].Acc.tolist())[2]
+    fsAcc = 238
+    accSen = 0.000244
+    toPlotX = toPlotX * accSen
+    toPlotY = toPlotY * accSen
+    toPlotZ = toPlotZ * accSen
+    xAxis = []
+
+    for i in range(toPlotX.size):
+        xAxis.append(i/fsAcc)
+    
+    fig, (ax1, ax2, ax3) = pyplot.subplots(3, sharex=True)
+    fig.suptitle('Accelerometer')
+    fig.text(0.5, 0.04, 'Time (s)', ha = 'center')
+    fig.text(0.04, 0.5, 'G', va = 'center', rotation = 'vertical')
+    ax1.plot(xAxis, toPlotX, linewidth = 0.5)
+    ax2.plot(xAxis, toPlotY, linewidth = 0.5)
+    ax3.plot(xAxis, toPlotZ, linewidth = 0.5)
+    pyplot.show()
+    
+elif sensor == 2:
+    toPlotX = pd.DataFrame(FallAllD.loc[indexToPlot].Gyr.tolist())[0]
+    toPlotY = pd.DataFrame(FallAllD.loc[indexToPlot].Gyr.tolist())[1]
+    toPlotZ = pd.DataFrame(FallAllD.loc[indexToPlot].Gyr.tolist())[2]
+    fsGyr = 238
+    gyrSen = 0.07
+    toPlotX = toPlotX * gyrSen
+    toPlotY = toPlotY * gyrSen
+    toPlotZ = toPlotZ * gyrSen
+    xAxis = []
+    for i in range(toPlotX.size):
+        xAxis.append(i/fsGyr)
+    
+    fig, (ax1, ax2, ax3) = pyplot.subplots(3, sharex=True)
+    fig.suptitle('Gyroscope')
+    fig.text(0.5, 0.04, 'Time (s)', ha = 'center')
+    fig.text(0.04, 0.5, '°/s', va = 'center', rotation = 'vertical')
+    ax1.plot(xAxis, toPlotX, linewidth = 0.5)
+    ax2.plot(xAxis, toPlotY, linewidth = 0.5)
+    ax3.plot(xAxis, toPlotZ, linewidth = 0.5)
+    pyplot.show()
+else:
+    toPlotX = pd.DataFrame(FallAllD.loc[indexToPlot].Mag.tolist())[0]
+    toPlotY = pd.DataFrame(FallAllD.loc[indexToPlot].Mag.tolist())[1]
+    toPlotZ = pd.DataFrame(FallAllD.loc[indexToPlot].Mag.tolist())[2]
+    fsMag = 80
+    magSen = 0.00014
+    toPlotX = toPlotX * magSen
+    toPlotY = toPlotY * magSen
+    toPlotZ = toPlotZ * magSen
+    xAxis = []
+    for i in range(toPlotX.size):
+        xAxis.append(i/fsMag)
+    
+    fig, (ax1, ax2, ax3) = pyplot.subplots(3, sharex=True)
+    fig.suptitle('Magnetometer')
+    fig.text(0.5, 0.04, 'Time (s)', ha = 'center')
+    fig.text(0.04, 0.5, 'Gauss', va = 'center', rotation = 'vertical')
+    ax1.plot(xAxis, toPlotX, linewidth = 0.5)
+    ax2.plot(xAxis, toPlotY, linewidth = 0.5)
+    ax3.plot(xAxis, toPlotZ, linewidth = 0.5)
+    pyplot.show()
+
+
