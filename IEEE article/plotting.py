e54b8239499765fff0c8fd421dc1d8a9d50e769e
Teddy Ordoñez <teddyonu@hotmail.com>
2023-07-01 20:28:41 -0300

    Plotting Acc and Gyr forward fall data

---
+++ IEEE article/plotting.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+FallAllD = pd.read_pickle(r'D:\teddy\Documents\UNIVALI\TCC III\TCC-iii\Dataframes\Dataframe1.pkl')
+activity = FallAllD[FallAllD['ActivityID'] == 104]
+accX = pd.DataFrame(activity.iloc[0].Acc.tolist())[0]
+accY = pd.DataFrame(activity.iloc[0].Acc.tolist())[1]
+accZ = pd.DataFrame(activity.iloc[0].Acc.tolist())[2]
+
+gyrX = pd.DataFrame(activity.iloc[0].Gyr.tolist())[0]
+gyrY = pd.DataFrame(activity.iloc[0].Gyr.tolist())[1]
+gyrZ = pd.DataFrame(activity.iloc[0].Gyr.tolist())[2]
+
+
+t_Acc = []
+for i in range(len(accX)):
+    t_Acc.append(i/238)
+
+
+fig, ax1 = plt.subplots(2, sharex=True)
+fig.suptitle('Forward fall while walking with recovery')
+
+ax1[0].plot(t_Acc, accX, linewidth = 0.25, label = 'X')
+ax1[0].plot(t_Acc, accY, linewidth = 0.25, label = 'Y')
+ax1[0].plot(t_Acc, accZ, linewidth = 0.25, label = 'Z')
+ax1[0].set_title('Accelerometer', {'fontsize': 8})
+ax1[0].set_ylabel('g')
+ax1[0].legend(loc = 'upper left', prop={'size': 6})
+
+ax1[1].plot(t_Acc, gyrX, linewidth = 0.25, label = 'X')
+ax1[1].plot(t_Acc, gyrY, linewidth = 0.25, label = 'Y')
+ax1[1].plot(t_Acc, gyrZ, linewidth = 0.25, label = 'Z')
+ax1[1].set_title('Gyroscope', {'fontsize': 8})
+ax1[1].set_ylabel('°/s')
+
+fig.supxlabel('Time (s)')
+plt.legend(loc = 'upper left', prop={'size': 6})
+plt.show()
